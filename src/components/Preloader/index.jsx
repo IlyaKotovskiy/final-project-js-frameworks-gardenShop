57ab13d600f1aacdee4393d935fbfafdcfc400d3
Ilya Kotovskiy <ilundrik026@gmail.com>
2023-12-28 14:56:42 +0300

    preloader works by loading the page

--- src/components/Preloader/index.jsx
+++ src/components/Preloader/index.jsx
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import s from "./Preloader.module.scss";
+import s from './Preloader.module.scss';
+import { useState, useEffect } from 'react';
 
 function Preloader() {
   const [isLoading, setLoading] = useState(true);
-  const location = useLocation();
 
   useEffect(() => {
-    const body = document.body;
-
-    setLoading(true);
-    body.style.overflow = 'hidden';
-
-    const timer = setTimeout(() => {
-      window.scrollTo(0, 0);
-      setLoading(false);
-      body.style.overflow = 'auto';
-    }, 500);
+    window.addEventListener('load', () => setLoading(false));
 
     return () => {
-      clearTimeout(timer);
+      window.removeEventListener('load', () => setLoading(false));
     };
-  }, [location.pathname]);
+  }, []);
 
   return (
-    <div className={s.preloader} style={{ display: isLoading ? "flex" : "none" }}>
+    <div
+      className={s.preloader}
+      style={{ display: isLoading ? 'flex' : 'none' }}
+    >
       <span className={s.loader}></span>
     </div>
   );
